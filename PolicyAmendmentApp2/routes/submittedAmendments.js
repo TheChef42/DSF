d8332aeadaa04361bc52278dbf0f4c6ee48af486
TheChef42 <danielflute@gmail.com>
2024-10-17 23:19:29 +0200

    d

--- PolicyAmendmentApp2/routes/submittedAmendments.js
+++ PolicyAmendmentApp2/routes/submittedAmendments.js
@@ -1,4 +1,3 @@
-// In routes/submittedAmendments.js
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
@@ -6,6 +5,10 @@ const db = require('../db');
 router.get('/', async (req, res) => {
     try {
         const [submittedAmendments] = await db.query('SELECT * FROM amendments');
+
+        // Log the retrieved data to the console
+        console.log('Fetched submitted amendments:', submittedAmendments);
+
         res.render('submittedAmendments', { submittedAmendments });
     } catch (error) {
         console.error('Error fetching submitted amendments:', error);
